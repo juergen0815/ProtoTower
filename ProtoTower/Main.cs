5286c51e88717019b718e3ed4c93b6d4e91351fb
juergen0815 <jurgen.schober@pointdesign.com>
2016-08-21 13:08:03 -0600

    Master got another method

--- ProtoTower/Main.cs
+++ ProtoTower/Main.cs
@@ -4,16 +4,21 @@ namespace ProtoTower
 {
 	public class Application
 	{
-		static String myMessage = "Hello World";
+		static String myMessage = "Hello World.";
 
 		public static void Main(string[] args)
 		{
-			new Application().HelloWorld();
+			new Application().init(args).HelloWorld();
 		}
 
 		public Application()
 		{
 		}
+
+		Application init(string[] args) {
+			// TODO: parse some args here.	
+			return this;
+		} 
 
 		void HelloWorld() {
 			Console.WriteLine(myMessage);
